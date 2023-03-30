9eb2c98f15d41616c679faac4144ac4d17dc8610
Apto <kkh1837@naver.com>
2023-03-30 17:26:17 +0900

    fs모듈을 통한 로그인 기능 구현

--- app/src/model/User.js
+++ app/src/model/User.js
@@ -4,15 +4,14 @@ const UserStorage = require("./UserStorage");
 
 class User  {
 
-    
-
     constructor(body){
         this.body = body;
     }
 
-    login(){
+    async login(){
         const client = this.body;
-        const { id, password } = UserStorage.getUserInfo(client.id);
+        const {id, password} = await UserStorage.getUserInfo(client.id);
+
         if (id) {
             if (id === client.id && password === client.password) {
                 return { success : true };
